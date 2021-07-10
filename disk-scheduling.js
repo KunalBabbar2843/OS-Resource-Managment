dbd47a799581c631050037d0aef700c85101b519
kunal babbar <kunalbabbar2843@gmail.com>
2021-07-10 22:29:10 +0530

    return type of result is added in all algorithm which has movments history

--- disk-scheduling.js
+++ disk-scheduling.js
@@ -1,209 +1,233 @@
 class DiskScheduling {
   constructor() {}
   firstComeFirstServe(request, head) {
-    let movement = 0;
-    console.log("<--FIRST COME FIRST-->");
+    let result = {
+      history: [head],
+      movement: 0,
+    };
     for (let block of request) {
-      movement += Math.abs(head - block);
-      console.log("FROM:" + head + " TO:" + block);
+      result.movement += Math.abs(head - block);
+      result.history.push(block);
       head = block;
     }
-    console.log("MOVEMENT:" + movement);
+    return result;
   }
   shortestSeekTime(request, head) {
-    let movement = 0;
-    console.log("<--SHORTEST SEEK TIME-->");
+    let result = {
+      history: [head],
+      movement: 0,
+    };
     while (request.length > 0) {
       let min_seek = 0;
       for (let i = 1; i < request.length; ++i) {
         if (Math.abs(head - request[min_seek]) > Math.abs(head - request[i]))
           min_seek = i;
       }
-      movement += Math.abs(head - request[min_seek]);
-      console.log("FROM:" + head + " TO:" + request[min_seek]);
+      result.movement += Math.abs(head - request[min_seek]);
+      result.history.push(request[min_seek]);
       head = request[min_seek];
       request.splice(min_seek, 1);
     }
-    console.log("MOVEMENT:" + movement);
+    return result;
   }
   Scan(request, head, end) {
-    let movement = 0,temp=head;
+    let result = {
+      history: {
+        left: [head],
+        right: [head],
+      },
+      movement: {
+        left: 0,
+        right: 0,
+      },
+    };
+    let temp = head;
     request.sort(function (a, b) {
       return a - b;
     });
     let m;
-    console.log("<--SCAN-->");
     for (m in request) {
       if (request[m] >= head) break;
     }
-    console.log("<--LEFT-->");
     for (let i = m - 1; i >= 0; --i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.left += Math.abs(head - request[i]);
+      result.history.left.push(request[i]);
       head = request[i];
     }
-    movement += head;
-    console.log("FROM:" + head + " TO:0");
+    result.movement.left += head;
+    result.history.left.push(0);
     head = 0;
     for (let i = m; i < request.length; ++i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.left += Math.abs(head - request[i]);
+      result.history.left.push(request[i]);
       head = request[i];
     }
-    console.log("MOVEMENT:" + movement);
-    console.log("<--RIGHT-->");
-    head=temp;
-    movement = 0;
+    head = temp;
     for (let i = m; i < request.length; ++i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.right += Math.abs(head - request[i]);
+      result.history.right.push(request[i]);
       head = request[i];
     }
-    movement += end - head;
-    console.log("FROM:" + head + " TO:" + end);
+    result.movement.right += end - head;
+    result.history.right.push(end);
     head = end;
     for (let i = m - 1; i >= 0; --i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.right += Math.abs(head - request[i]);
+      result.history.right.push(request[i]);
       head = request[i];
     }
-    console.log("MOVEMENT:" + movement);
+    return result;
   }
   Cscan(request, head, end) {
-    let movement = 0,temp=head;
+    let result = {
+      history: {
+        left: [head],
+        right: [head],
+      },
+      movement: {
+        left: 0,
+        right: 0,
+      },
+    };
+    let temp = head;
     request.sort(function (a, b) {
       return a - b;
     });
     let m;
-    console.log("<--C-SCAN-->");
     for (m in request) {
       if (request[m] >= head) break;
     }
-    console.log("<--LEFT-->");
     for (let i = m - 1; i >= 0; --i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.left += Math.abs(head - request[i]);
+      result.history.left.push(request[i]);
       head = request[i];
     }
-    movement += head;
-    console.log("FROM:" + head + " TO:0");
+    result.movement.left += head;
+    result.history.left.push(0);
     head = 0;
-    movement+=end;
-    console.log("FROM:" + head + " TO:" + end);
+    result.movement.left += end;
+    result.history.left.push(end);
     head = end;
     for (let i = request.length - 1; i >= m; --i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.left += Math.abs(head - request[i]);
+      result.history.left.push(request[i]);
       head = request[i];
     }
-    console.log("MOVEMENT:" + movement);
-    console.log("<--RIGHT-->");
-    head=temp;
-    movement = 0;
+    head = temp;
     for (let i = m; i < request.length; ++i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.right += Math.abs(head - request[i]);
+      result.history.right.push(request[i]);
       head = request[i];
     }
-    movement += end - head;
-    console.log("FROM:" + head + " TO:" + end);
+    result.movement.right += end - head;
+    result.history.right.push(end);
     head = end;
-    movement+=end;
-    console.log("FROM:" + head + " TO:0");
+    result.movement.right += end;
+    result.history.right.push(0);
     head = 0;
     for (let i = 0; i < m; ++i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.right += Math.abs(head - request[i]);
+      result.history.right.push(request[i]);
       head = request[i];
     }
-    console.log("MOVEMENT:" + movement);
+    return result;
   }
   Look(request, head, end) {
-    let movement = 0,temp=head;
+    let result = {
+      history: {
+        left: [head],
+        right: [head],
+      },
+      movement: {
+        left: 0,
+        right: 0,
+      },
+    };
+    let temp = head;
     request.sort(function (a, b) {
       return a - b;
     });
     let m;
-    console.log("<--LOOK-->");
     for (m in request) {
       if (request[m] >= head) break;
     }
-    console.log("<--RIGHT-->");
     for (let i = m; i < request.length; ++i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.right += Math.abs(head - request[i]);
+      result.history.right.push(request[i]);
       head = request[i];
     }
     for (let i = m - 1; i >= 0; --i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.right += Math.abs(head - request[i]);
+      result.history.right.push(request[i]);
       head = request[i];
     }
-    console.log("MOVEMENT:" + movement);
-    console.log("<--LEFT-->");
-    head=temp;
-    movement = 0;
+    head = temp;
     for (let i = m - 1; i >= 0; --i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.left += Math.abs(head - request[i]);
+      result.history.left.push(request[i]);
       head = request[i];
     }
     for (let i = m; i < request.length; ++i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.left += Math.abs(head - request[i]);
+      result.history.left.push(request[i]);
       head = request[i];
     }
-    console.log("MOVEMENT:" + movement);
+    return result;
   }
   Clook(request, head, end) {
-    let movement = 0,temp=head;
+    let result = {
+      history: {
+        left: [head],
+        right: [head],
+      },
+      movement: {
+        left: 0,
+        right: 0,
+      },
+    };
+    let temp = head;
     request.sort(function (a, b) {
       return a - b;
     });
     let m;
-    console.log("<--C-LOOK-->");
     for (m in request) {
       if (request[m] >= head) break;
     }
-    console.log("<--RIGHT-->");
     for (let i = m; i < request.length; ++i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.right += Math.abs(head - request[i]);
+      result.history.right.push(request[i]);
       head = request[i];
     }
     for (let i = 0; i < m; ++i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.right += Math.abs(head - request[i]);
+      result.history.right.push(request[i]);
       head = request[i];
     }
-    console.log("MOVEMENT:" + movement);
-    movement = 0;
-    console.log("<--LEFT-->");
-    head=temp;
+    head = temp;
     for (let i = m - 1; i >= 0; --i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.left += Math.abs(head - request[i]);
+      result.history.left.push(request[i]);
       head = request[i];
     }
     for (let i = request.length - 1; i >= m; --i) {
-      movement += Math.abs(head - request[i]);
-      console.log("FROM:" + head + " TO:" + request[i]);
+      result.movement.left += Math.abs(head - request[i]);
+      result.history.left.push(request[i]);
       head = request[i];
     }
-    console.log("MOVEMENT:" + movement);
+    return result;
   }
   ShowAllDiskAlgo(request, head, end, dir) {
-    this.firstComeFirstServe([...request], head);
-    this.shortestSeekTime([...request], head);
-    this.Scan([...request], head, end, dir);
-    this.Look([...request], head, end, dir);
-    this.Cscan([...request], head, end, dir);
-    this.Clook([...request], head, end, dir);
+    console.table(this.firstComeFirstServe([...request], head));
+    console.table(this.shortestSeekTime([...request], head));
+    console.table(this.Scan([...request], head, end));
+    console.table(this.Look([...request], head, end));
+    console.table(this.Cscan([...request], head, end));
+    console.table(this.Clook([...request], head, end));
   }
 }
 
 let disk = new DiskScheduling();
-let request = [10, 22, 20, 2, 40,6,38];
-let head = 20,
-    end = 199;
+let request = [40, 124, 22, 86, 132, 60, 185, 17];
+let head = 50,
+  end = 199;
 disk.ShowAllDiskAlgo(request, head, end);
